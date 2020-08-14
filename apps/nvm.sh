60f0121d5d043ffdee9c492e322b4a18608271e2
Corey O'Donnell <me@coreyodonnell.com>
2020-08-13 23:24:23 -0400

    Update dots to install i3

--- apps/nvm.sh
+++ apps/nvm.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -s "$HOME/.nvm/nvm.sh" ]]; then
     echo "Installing NVM..."
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash
 else
     echo "NVM already installed"
 fi
