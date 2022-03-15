4d4b7cb76eb90ac7abe53d4fa6e59c68e5aadc48
Corey <corey@onlyalt.com>
2022-03-14 22:54:18 -0400

    add some helpful scripts

---
+++ bin/.local/bin/delete-branches.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git branch |
+    grep --invert-match '\*' |
+    cut -c 3- |
+    fzf --multi --preview="git log {} --" |
+    xargs --no-run-if-empty git branch --delete --force
+
---
+++ scripts/git-config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Push to current branch without setting upstream
+git config --global push.default current
+
