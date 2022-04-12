fdf5f99b6a5c18c6953b9d211ab2b517e0323aac
Corey O'Donnell <corey@onlyalt.com>
2022-04-12 00:08:26 -0400

    Neovim setup (#7)
    * update folders for stow
    
    * feat: add script for vim-plug
    
    * feat: change prefix, add default terminal setting in tmux
    
    * feat: Add basic neovim setup, lspconfig, git, lspsaga, and git
    
    * feat: Neovim in a nearly usable state, fuzzy file finder, snytax highlighting, lsp

---
+++ scripts/dev-bootstrap.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Bootstrap Computer
+
+# Install homebrew
+
+brew install keycastr --cask
+brew install git
+brew install karabiner
+brew install alfred --cask
+brew install num --cask
+
+# Programming Languages
+brew install golang
+
+# Bootstrap Terminal
+brew install iterm2 --cask
+brew install tmux
+brew install neovim
+
+
+# Neovim Dependencies
+brew install ripgrep # Telescope
+brew install fd # Telescope
+
+# Notes and TODOS Section
+## Streaming
+### brew install keycastr --cask (Display keys pressed on screen)
+### Cursor Pro (Cursor rings to improve visibility)
+
+## Browsers
+### Min Browser
+### Firefox
+### Brave
+
+## No script
+### HTTP Clients (Paw, Postman)
+### iStats Menus
+
